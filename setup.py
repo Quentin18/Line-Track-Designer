21f46df6b39a7ee46ee89afceea35a2121486ac6
Quentin18 <quentindeschamps18@orange.fr>
2020-06-22 16:57:34 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,24 +1,37 @@
+"""
+* Install with pip (recommended):
+    pip3 install .
+* Install with setuptools:
+    python3 setup.py install
+* Run tests:
+    python3 setup.py pytest
+"""
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="line-track-designer",
-    version="0.0.1",
-    author="Quentin Deschamps",
-    author_email="quentindeschamps18@gmail.com",
-    description="A library to design line following tracks for robots",
+    name='line-track-designer',
+    version='0.0.1',
+    author='Quentin Deschamps',
+    author_email='quentindeschamps18@gmail.com',
+    description='A library to design line following tracks for robots',
+    keywords='line follow robot track',
+    license='MIT',
+    url='https://github.com/Quentin18/Line-Track-Designer',
+    packages=['line_track_designer'],
+    platforms='any',
+    python_requires='>=3',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Quentin18/Line-Track-Designer",
-    packages=["line_track_designer"],
     include_package_data=True,
-    # install_requires=[],
+    install_requires=['numpy'],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    setup_requires=['pytest-runner'],
+    tests_require='pytest',
 )
